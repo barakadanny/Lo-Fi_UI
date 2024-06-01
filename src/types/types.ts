eeb10079f9b3bd31df19d8d8419b1efd9b97e715
barakadanny <barakadan421@gmail.com>
2024-06-02 01:07:12 +0300

    types file

---
+++ src/types/types.ts
@@ -0,0 +1,56 @@
+export interface RentalToolData {
+  Tool: number;
+  Name: string;
+  Status: string;
+  Duration: string;
+  ProfileImage: string;
+}
+
+export interface NameType {
+  icon: string;
+  name: string;
+}
+
+export interface RestockItemsData {
+  Tool: number;
+  Equipment: string;
+  Status: string;
+}
+
+export interface WorkOrderData {
+  completed: number;
+  inProgress: number;
+  due: number;
+}
+
+export interface ToolData {
+  icon: React.ReactNode;
+  name: string;
+  score: number;
+}
+
+export interface ReceptionSummaryData {
+  icon: string;
+  name: string;
+  count: number;
+}
+
+export const receptionSummaryHeaders: string[] = ["Icon", "Name", "Count"];
+
+export const missingItemsHeaders: string[] = ["Tool", "Name", "Action"];
+
+export const rentalToolsHeaders: string[] = [
+  "Work Order",
+  "Tool",
+  "Name",
+  "Status",
+  "Duration",
+  "Action",
+];
+
+export const restockItemsHeaders: string[] = [
+  "Tool",
+  "Equipment",
+  "Status",
+  "Action",
+];

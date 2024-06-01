59f5c21d3181967af11d2f72e86d0522ff41a275
barakadanny <barakadan421@gmail.com>
2024-06-02 02:36:48 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./admin/pages/Home";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>

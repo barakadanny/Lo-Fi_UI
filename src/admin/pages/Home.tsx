8623de0346909be10cf2c9aee66c6266db225781
barakadanny <barakadan421@gmail.com>
2024-06-02 02:36:35 +0300

    Home Page

--- src/admin/pages/Home.tsx
+++ src/admin/pages/Home.tsx
@@ -1,17 +1,24 @@
+import Container from "../../shared/components/UIElements/Container";
+// import GridLayout from "../../shared/components/UIElements/GridLayout";
+
 import RentalTools from "../components/RentalTools";
 import MissingItems from "../components/MissingItems";
 import RestockItems from "../components/RestockItems";
 import WorkOrderStatus from "../components/WorkOrderStatus";
 import ToolsAndEquipmentsAvailability from "../components/ToolsAndEquipmentsAvailability";
+import ReceptionSummary from "../components/ReceptionSummary";
 
 export default function Home() {
   return (
-    <div>
-      <RentalTools />
-      <MissingItems />
-      <RestockItems />
-      <WorkOrderStatus />
-      <ToolsAndEquipmentsAvailability />
-    </div>
+    <Container>
+      <div className="grid grid-cols-5 gap-4">
+        <RentalTools />
+        <WorkOrderStatus />
+        <MissingItems />
+        <ToolsAndEquipmentsAvailability />
+        <RestockItems />
+        <ReceptionSummary />
+      </div>
+    </Container>
   );
 }

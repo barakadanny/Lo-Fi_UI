a446c1f6830721312408c780de4315185adc381e
barakadanny <barakadan421@gmail.com>
2024-06-02 02:35:25 +0300

    ReceptionSummary

---
+++ src/admin/components/ReceptionSummary.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CardContainer from "../../shared/components/UIElements/CardContainer";
+import CardInformation from "../../HOC/CardInformation";
+import { getReception } from "../../api/receptionSummary";
+
+const ReceptionSummary: React.FC = () => {
+  return (
+    <CardContainer className="col-span-2">
+      <CardInformation
+        headers={{
+          name: "Reception Summary",
+          icon: "",
+        }}
+        type="container"
+        values={getReception()}
+      />
+    </CardContainer>
+  );
+};
+
+export default ReceptionSummary;

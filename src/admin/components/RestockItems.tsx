d0868f2804c7b3c893efc8357756a7bee4ddca3a
barakadanny <barakadan421@gmail.com>
2024-06-02 02:36:25 +0300

    RestockItems

--- src/admin/components/RestockItems.tsx
+++ src/admin/components/RestockItems.tsx
@@ -1,49 +1,18 @@
-import React from "react";
-import Table from "../../shared/components/UIElements/Table";
-
-interface RestockItemsData {
-  Tool: number;
-  Equipment: string;
-  Status: string;
-}
-
-interface RestockItemsProps {
-  headers: string[];
-  data: RestockItemsData[];
-}
-
-const dummy_data: RestockItemsData[] = [
-  {
-    Tool: 6465,
-    Equipment: "Tool One",
-    Status: "Low",
-  },
-  {
-    Tool: 6466,
-    Equipment: "Tool Two",
-    Status: "Medium",
-  },
-  {
-    Tool: 6467,
-    Equipment: "Tool Three",
-    Status: "none",
-  },
-];
-
-const restockItemsHeaders: string[] = ["Tool", "Equipment", "Status"];
-
-const RestockItemsComponent: React.FC<RestockItemsProps> = ({
-  headers,
-  data,
-}) => {
-  return <Table headers={headers} data={data} />;
-};
+import CardTable from "../../HOC/CardTable";
+import { getItemsToRestock } from "../../api/restockItems";
+import CardContainer from "../../shared/components/UIElements/CardContainer";
 
 export default function RestockItems() {
   return (
-    <RestockItemsComponent
-      headers={restockItemsHeaders} // Pass updated headers
-      data={dummy_data}
-    />
+    <CardContainer className="col-span-3">
+      <CardTable
+        headers={{
+          name: "Restock Items",
+          icon: "",
+        }}
+        type="table"
+        values={getItemsToRestock()}
+      />
+    </CardContainer>
   );
 }

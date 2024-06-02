47d628465ddd6420b8ef1e741df9184a5788c08c
barakadanny <barakadan421@gmail.com>
2024-06-02 15:46:34 +0300

    Refactor Table

--- src/shared/components/UIElements/Table.tsx
+++ src/shared/components/UIElements/Table.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import Profile from "./../UIElements/Profile";
-
-interface RentalToolData {
-  Tool: number;
-  Name: string;
-  Status: string;
-  Duration: string;
-  ProfileImage: string;
-  [key: string]: any; // Update TableRow interface to allow JSX.Element for images
-}
-
-interface TableProps {
-  headers: string[];
-  data: RentalToolData[];
-}
+import StatusIndicator from "./StatusIndicator";
+import { RentalToolData, TableProps } from "../../../types/types";
 
 const Table: React.FC<TableProps> = ({ headers, data }) => (
-  <table className="w-full text-sm text-left ">
+  <table className="w-full text-sm text-left">
     <thead className="text-base text-gray-700 border-b">
       <tr>
         {headers.map((header) => (
@@ -51,29 +39,7 @@ const Table: React.FC<TableProps> = ({ headers, data }) => (
                   </span>
                 </div>
               ) : header === "Status" ? (
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <span
-                    style={{
-                      width: "10px",
-                      height: "10px",
-                      borderRadius: "50%",
-                      marginRight: "8px",
-                      backgroundColor:
-                        row[header] === "Completed"
-                          ? "green"
-                          : row[header] === "In Progress"
-                          ? "blue"
-                          : row[header] === "Not Started"
-                          ? "red"
-                          : row[header] === "Low"
-                          ? "orange"
-                          : row[header] === "None"
-                          ? "red"
-                          : "gray",
-                    }}
-                  ></span>
-                  {row[header]}
-                </div>
+                <StatusIndicator status={row[header]} />
               ) : (
                 row[header]
               )}

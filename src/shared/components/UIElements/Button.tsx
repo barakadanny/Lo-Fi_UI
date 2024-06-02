e4714afd1710efc299bd06eef214f1837125c595
barakadanny <barakadan421@gmail.com>
2024-06-02 11:00:56 +0300

    Update Button component

--- src/shared/components/UIElements/Button.tsx
+++ src/shared/components/UIElements/Button.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-
-interface ButtonProps {
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
-  disabled?: boolean;
-  type?: "button" | "submit" | "reset";
-}
+import { ButtonProps } from "../../../types/types";
 
 const Button: React.FC<ButtonProps> = ({
   children,
